467a72a5fa81f4614bcfc1691c82ac7be3297113
Matthew Vandenberg <mv5903@gmail.com>
2022-06-01 23:14:01 -0700

    some stuff

--- src/game/Pawn.java
+++ src/game/Pawn.java
@@ -1,13 +1,10 @@
 package game;
 
 public class Pawn extends Piece {
-	String letterForPiece = "P";
 
 	public Pawn(int x, int y) {
 		super(x, y);
 		this.type = PieceType.Pawn;
 	}
-	public void move() {
-		this.locationY --;
-	}
+	
 }
--- src/game/Piece.java
+++ src/game/Piece.java
@@ -3,10 +3,12 @@ package game;
 public class Piece {
 	int locationX, locationY;
 	PieceType type;
+	Board.Tile locatedOn;
 	
 	public Piece(int x, int y) {
 		this.locationX = x;
 		this.locationY = y;
+		locatedOn = null;
 	}
 	
 	public int getLocationX(){
@@ -17,6 +19,22 @@ public class Piece {
 	}
 	public PieceType getType(){
 		return type;
+	}
+	public void move(Board.Tile tile) {
+		if (locatedOn == null) {
+			locatedOn = tile;
+			locatedOn.setText(getRep());
+			return;
+		}
+		locatedOn.setText("");
+		locatedOn = tile;
+		locationX = tile.x;
+		locationY = tile.y;
+		locatedOn.setText(getRep());
+	}
+	
+	public String getRep() {
+		return type.toString();
 	}
 	
 }
