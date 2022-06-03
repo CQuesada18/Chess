bc22c4b507f7af201c30a44e8214bec3cb6d7a18
Matthew Vandenberg <mv5903@gmail.com>
2022-06-02 21:18:35 -0700

    IMages

--- src/poker/Table.java
+++ src/poker/Table.java
@@ -3,6 +3,7 @@ package poker;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.util.LinkedList;
 
@@ -10,7 +11,10 @@ import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Table extends JPanel {
+import poker.Const.Suit;
+import poker.Const.Value;
+
+public class Table extends JPanel{
 	// Left side things
 	JPanel left;
 	JLabel scoreTitle = new JLabel("Scores");
@@ -20,6 +24,9 @@ public class Table extends JPanel {
 	
 	// Middle things
 	JPanel middle;
+	LinkedList<JPanel> centerCards = new LinkedList<>();
+	JPanel leftCard = new JPanel();
+	JPanel rightCard = new JPanel();
 	
 	// Right things
 	JPanel right;
@@ -37,9 +44,13 @@ public class Table extends JPanel {
 		right = new JPanel();
 		
 		left.setPreferredSize(new Dimension((int)(Main.WIDTH * .2), Main.HEIGHT));
+		left.setBackground(Color.DARK_GRAY);
 		left.setLayout(null);
 		middle.setPreferredSize(new Dimension((int)(Main.WIDTH * .6), Main.HEIGHT));
+		middle.setLayout(new BorderLayout());
+		middle.setBackground(Color.DARK_GRAY);
 		right.setPreferredSize(new Dimension((int)(Main.WIDTH * .2), Main.HEIGHT));
+		right.setBackground(Color.DARK_GRAY);
 		
 		left.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 		middle.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
@@ -59,15 +70,35 @@ public class Table extends JPanel {
 		
 		
 		scoresPanel = Score.getScoresAsPanel(playerScores);
+		scoresPanel.setBackground(Color.DARK_GRAY);
 		scoresPanel.setBounds(25, 100, (int) (Main.WIDTH * .15), (int) (Main.HEIGHT * .3));
 		left.add(scoreTitle);
 		left.add(scoresPanel);
 		
 		pot = new Pot();
-		
-		
-		
 		for (JLabel score: playerScores) left.add(score);
+		
+		// ALL Middle Stuff
+		JPanel centerCardPanel = new JPanel();
+		centerCardPanel.setBackground(Color.DARK_GRAY);
+		centerCards.add(CardDisplay.displayCard(new Card(Suit.SPADES, Value.ACE)));
+		centerCards.add(CardDisplay.displayCard(new Card(Suit.SPADES, Value.EIGHT))); 
+		centerCards.add(CardDisplay.displayCard(new Card(Suit.DIAMONDS, Value.TWO))); 
+		centerCards.add(CardDisplay.displayCard(new Card(Suit.CLUBS, Value.NINE)));
+		centerCards.add(CardDisplay.displayCard(new Card(Suit.HEARTS, Value.KING))); 
+		centerCardPanel.setSize((int) (Main.WIDTH * .6), (int) (Main.HEIGHT * .3));
+		
+		for (JPanel centerCard: centerCards) centerCardPanel.add(centerCard);
+		middle.add(centerCardPanel, BorderLayout.NORTH);
+		
+		JPanel handCardPanel = new JPanel();
+		handCardPanel.setBackground(Color.DARK_GRAY);
+		leftCard = CardDisplay.displayCard(new Card(Suit.DIAMONDS, Value.SEVEN));
+		rightCard = CardDisplay.displayCard(new Card(Suit.HEARTS, Value.FIVE));
+		handCardPanel.add(leftCard);
+		handCardPanel.add(rightCard);
+		
+		middle.add(handCardPanel, BorderLayout.SOUTH);
 		this.add(left, BorderLayout.LINE_START);
 		this.add(middle, BorderLayout.CENTER);
 		this.add(right, BorderLayout.LINE_END);
